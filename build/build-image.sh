a53d1e18baf5b53db18edc9b71aee2298606cce4
gmorpheme <github@gmorpheme.net>
2015-12-04 15:54:12 +0000

    Correct circle env var.

--- build/build-image.sh
+++ build/build-image.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION=0.0.1-${CIRCLE_VERSION}
+VERSION=0.0.1-${CIRCLE_BUILD_NUM}
 
 docker build -t curvelogic/simianarmy-docker:$VERSION .
 docker tag curvelogic/simianarmy-docker:$VERSION curvelogic/simianarmy-docker:latest 
--- build/publish-image.sh
+++ build/publish-image.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION=0.0.1-${CIRCLE_VERSION}
+VERSION=0.0.1-${CIRCLE_BUILD_NUM}
 
 docker push curvelogic/simianarmy-docker:$VERSION
 docker push curvelogic/simianarmy-docker:latest
